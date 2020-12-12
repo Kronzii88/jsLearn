625eebc7377f1744639d8c30f04c48cbd227d99b
Kronzii88 <ezyh.arofi@gmail.com>
2020-12-12 08:20:01 +0700

    menambah filterTodo

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ const clearButton = document.querySelector("#clear-todos");
 
 //function
 addTodo = (e) => {
+    e.preventDefault();
+
     if(todoInput.value) {
-        e.preventDefault();
-    
         // membuat virtual DOM untuk element li
         const li = document.createElement("li");
-        li.className = "list-group-item d-flex justify-content-between align-items-center mb-1";
+        li.className = "todo-item list-group-item d-flex justify-content-between align-items-center mb-1"; //penambahan className todo-item untuk function filterTodo 
         li.appendChild(document.createTextNode(todoInput.value));
         
         const a = document.createElement("a");
         a.href = "#";
-        a.className = "badge badge-danger delete-todo"; //penambahan className delete-todo untuk function deleteTodo    
+        a.className = "badge badge-danger delete-todo"; //penambahan className delete-todo untuk function deleteTodo
         a.innerHTML = "Delete";
         li.appendChild(a);
         
@@ -44,7 +44,29 @@ clearTodo = () => {
     console.log("clicked");
     todoList.innerHTML = "";
 }
+
+filterTodo = (e) => {
+    //menggunakan method index of
+    e.preventDefault();
+    const isi = e.target.value.toLowerCase();
+    const todoItem = document.querySelectorAll(".todo-item");
+    todoItem.forEach((item) => {
+        const itemText = item.firstChild.textContent.toLowerCase();
+        //memakai method indexOf()
+        if(itemText.indexOf(isi) === -1) {
+            item.setAttribute("style", "display: none !important;");
+        } else {
+            item.setAttribute("style", "display: block;");
+        }
+        console.log(itemText);
+    })
+
+    // console.log(todoItem);
+
+}
+
 //event listener
 todoForm.addEventListener("submit", addTodo);
 todoList.addEventListener("click", deleteTodo);
-clearButton.addEventListener("click", clearTodo)
+clearButton.addEventListener("click", clearTodo);
+filterInput.addEventListener("keyup", filterTodo);
